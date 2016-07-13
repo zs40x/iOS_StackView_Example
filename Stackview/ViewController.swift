8b7ce887259bb1c7d3895357b9b70e4e46591d43
Stefan Mehnert <StefanMehnert@gmail.com>
2016-07-13 16:00:11 +0200

    Initial Commit

---
+++ Stackview/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Stackview
+//
+//  Created by Stefan Mehnert on 13/07/16.
+//  Copyright © 2016 Stefan Mehnert. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
